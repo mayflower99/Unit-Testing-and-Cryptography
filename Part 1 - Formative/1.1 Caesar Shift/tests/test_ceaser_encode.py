8aad7071e64c593ed96ee48875b4cc4c0cdc2afc
200917 <christian.austin.jackson@gmail.com>
2024-09-10 10:37:55 -0400

    eoc 9/9/2024 ceaser works

--- Part 1 - Formative/1.1 Caesar Shift/tests/test_ceaser_encode.py
+++ Part 1 - Formative/1.1 Caesar Shift/tests/test_ceaser_encode.py
@@ -11,14 +11,17 @@ class TestInsertString(TestCase):
     def test_blank_equals_blank(self):
         self.assertEqual(ce("", 10), "")
     def test_n_equals_0(self):
-        self.assertEqual(ce("thequickbrownfoxjumpsoverthelazydog", 0), cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 0))
+        self.assertEqual("thequickbrownfoxjumpsoverthelazydog", cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 0))
     def test_n_equals_256(self):
-        self.assertEqual(ce("thequickbrownfoxjumpsoverthelazydog", 256), cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 256))
+        self.assertEqual("thequickbrownfoxjumpsoverthelazydog", cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 256))
     def test_n_equals_negitive(self):
-        self.assertEqual(ce("thequickbrownfoxjumpsoverthelazydog", -256), cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), -256))
+        self.assertEqual("thequickbrownfoxjumpsoverthelazydog", cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), -256))
     def test_spaces(self):
-        self.assertEqual(ce("the quick brown fox jumps over the lazy dog", 256), cd(ce("the quick brown fox jumps over the lazy dog", 0), 256))
+        self.assertEqual("the quick brown fox jumps over the lazy dog", cd(ce("the quick brown fox jumps over the lazy dog", 0), 256))
     def test_capitals(self):
-        self.assertEqual(ce("thequickbrownfoxjumpsoverthelazydog", 256), cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 256))
+        self.assertEqual("TheQuickBrownFoxJumpsOverTheLazyDog", cd(ce("TheQuickBrownFoxJumpsOverTheLazyDog", 50), 50))
     def test_special_charaters(self):
-        self.assertEqual(ce("thequickbrownfoxjumpsoverthelazydog", 256), cd(ce("thequickbrownfoxjumpsoverthelazydog", 0), 256))
+        self.assertEqual("!@#$%^&*()_+-={}|:\"<>?[]\\;',./'", cd(ce("!@#$%^&*()_+-={}|:\"<>?[]\\;',./'", 50), 50))
+    def test_all_values_of_n(self):
+        for n in range(-10000,10000):
+            self.assertEqual("!@#$%^&*()_+-={}|:\"<>?[]\\;',./'The Quick Brown Fox Jumps Over The Lazy Dog", cd(ce("!@#$%^&*()_+-={}|:\"<>?[]\\;',./'The Quick Brown Fox Jumps Over The Lazy Dog", n), n))
