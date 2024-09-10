3ea54ff0b10f9cbf941d0319d492d98578287382
200917 <christian.austin.jackson@gmail.com>
2024-09-10 11:11:45 -0400

    eoc 9/10/2024

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -1,12 +1,26 @@
 # Read the instructions to see what you need to do here!
 
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def validate_input(func):
+    def validate_string(string):
+        string = string.upper()
+        new_string = ""
+        for i in string:
+            if ord(i) in range(65,91):
+                new_string = new_string + i
+        return new_string
 
-
+    def wrapper(*args, **kwargs):
+        text = args[0]
+        text = validate_string(text)
+        codebet = args[1]
+        return func(text, codebet)
+    return wrapper
+@validate_input
 def sub_encode(text, codebet):
     return "".join([codebet[alpha.index(x)] for x in text])
 
-
+@validate_input
 def sub_decode(text, codebet):
     return "".join([alpha[codebet.index(x)] for x in text])
 
@@ -14,7 +28,7 @@ def sub_decode(text, codebet):
 test = "HELLOWORLD"
 cipher_alphabet = "WJKUXVBMIYDTPLHZGONCRSAEFQ"
 enc = sub_encode(test, cipher_alphabet)
-dec = sub_decode(enc, cipher_alphabet)
+dec = sub_decode(str(enc), cipher_alphabet)
 print(enc)
 print(dec)
 # If this worked, dec should be the same as test!
